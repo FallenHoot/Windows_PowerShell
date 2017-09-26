4f0f5ed9e2a7d5f7767c8638a701f79b20eeb26a
Zach <Fishinrocks@aol.com>
2017-09-26 13:59:34 +0200

    Scripts

---
+++ Get_TaskManager.ps1
@@ -0,0 +1,17 @@
+Function Get-CPUProcess
+{
+$properties=@(
+    @{Name="Name"; Expression = {$_.name}},
+    @{Name="PID"; Expression = {$_.IDProcess}},
+    @{Name="CPU (%)"; Expression = {$_.PercentProcessorTime}},
+    @{Name="Memory (MB)"; Expression = {[Math]::Round(($_.workingSetPrivate / 1mb),2)}}
+    @{Name="Disk (MB)"; Expression = {[Math]::Round(($_.IODataOperationsPersec / 1mb),2)}}
+)
+$ProcessCPU = Get-WmiObject -class Win32_PerfFormattedData_PerfProc_Process |
+    Select-Object $properties |
+    Sort-Object "CPU (%)" -desc |
+    Select-Object -First 5 |
+    Format-Table -AutoSize
+    $ProcessCPU
+}
+Get-CPUProcess
